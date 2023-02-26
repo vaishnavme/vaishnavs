da0cd4c8636fcd73bc47244b778e8eb117d52f5d
Vaishnav <vaishnavchandurkar12@gmail.com>
2023-02-26 11:14:43 +0530

    ADD: added layout prop

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
 import type { AppProps } from "next/app";
+import type { ReactElement, ReactNode } from "react";
+import type { NextPage } from "next";
 import { ThemeProvider } from "next-themes";
 import { Layout } from "@/components/Layout";
 import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || ((page) => <Layout>{page}</Layout>);
   return (
     <ThemeProvider attribute="class">
