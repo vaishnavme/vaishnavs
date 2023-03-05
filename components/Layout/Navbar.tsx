c79ba8ac00a3029194fd06001fa79872c58364e8
Vaishnav <vaishnavchandurkar12@gmail.com>
2023-03-05 13:50:26 +0530

    ADD: added blockquote

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -1,5 +1,6 @@
 import { useTheme } from "next-themes";
 import Image from "next/image";
+import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import Icon from "../UIElements/SVGIcons";
 
@@ -12,19 +13,23 @@ function Navbar() {
   return (
     <div className="max-w-5xl mx-auto px-4 md:px-6 lg:px-10">
       <nav className="flex items-center justify-between w-full max-w-5xl py-2">
-        <div className="flex items-center gap-2">
-          <Image
-            src="/images/vaishnav_chandurkar.jpeg"
-            width={40}
-            height={40}
-            alt="Vaishnav Chandurkar"
-            className="rounded-full object-cover w-10 h-10"
-          />
-          <div>
-            <h1 className="font-normal text-base leading-5">Vaishnav</h1>
-            <p className="text-xs text-zinc-800 dark:text-zinc-100">& I code</p>
+        <Link href="/" className="group">
+          <div className="flex items-center gap-2">
+            <Image
+              src="/images/vaishnav_chandurkar.jpeg"
+              width={40}
+              height={40}
+              alt="Vaishnav Chandurkar"
+              className="rounded-full object-cover w-10 h-10 group-hover:shadow-2xl"
+            />
+            <div>
+              <h1 className="font-normal text-base leading-5">Vaishnav</h1>
+              <p className="text-xs text-zinc-800 dark:text-zinc-100">
+                & I code
+              </p>
+            </div>
           </div>
-        </div>
+        </Link>
         <div>
           <button
             onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
