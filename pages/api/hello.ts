ddb82f7472ffdd056f441d28fc68bacb2d953a64
Vaishnav <vaishnavchandurkar12@gmail.com>
2023-03-12 22:56:47 +0530

    ADD: site info api

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
+
+const handler = async(
+  req: NextApiRequest,
+  resp: NextApiResponse
+) => {
+  const siteInfo = {
+    name: 'Vaishnav Chandurkar',
+    job: 'Software Engineer @PeerlistHQ',
+    site: 'https://vaishnavs.vercel.app',
+    social: {
+      peerlist: 'https://peerlist.io/vaishnavs',
+      twitter: 'https://twitter.com/vaishnavs0'
+    },
+    sayHello: 'https://peerlist/inbox'
+  }
+  resp.status(200).send(siteInfo);
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'Vaishnav Chandurkar' })
-}
+export default handler;
