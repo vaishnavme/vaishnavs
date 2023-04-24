6cff875f096643ffd5f60b027cc5141afbb05405
Vaishnav <vaishnavchandurkar12@gmail.com>
2023-04-24 12:36:53 +0530

    ADD: basic about page

---
+++ pages/about.tsx
@@ -0,0 +1,18 @@
+import MetaSEO from "@/components/UIElements/MetaSEO";
+
+const About = () => {
+  return (
+    <>
+      <MetaSEO
+        title="Blog | Vaishnav Chandurkar"
+        description="Software Engineer @Peerlist"
+        keywords="Engineer, Frontend Developer, Developer"
+        ogImage="/images/vaishnav_og.png"
+      />
+
+      <div>✍️ writing in progress</div>
+    </>
+  );
+};
+
+export default About;
