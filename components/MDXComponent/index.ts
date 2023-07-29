023aebe390d6e63c5239612be4f22e0e73924f5a
Vaishnav <vaishnavchandurkar12@gmail.com>
2023-07-29 23:03:44 +0530

    add: migrate blog post

--- components/MDXComponent/index.ts
+++ components/MDXComponent/index.ts
@@ -2,11 +2,13 @@ import Image from "next/image";
 import Step from "./Step";
 import StepCheck from "./StepCheck";
 import StepLarge from "./StepLarge";
+import Embed from "./Embed";
 
 const MDXComponents = {
   Step,
   StepCheck,
   StepLarge,
+  Embed,
   Image
 }
 
