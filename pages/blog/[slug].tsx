4f22d41eb55f523f318886bd90e224a183dfcb6f
Vaishnav Chandurkar <42497931+vaishnavme@users.noreply.github.com>
2023-07-29 23:54:44 +0530

    add: meta date for blog page (#10)

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -5,6 +5,7 @@ import MDXComponents from "@/components/MDXComponent";
 import blogService from "@/lib/blog.services";
 import { TPostFrontmatter } from "@/utils/global.types";
 import helpers from "@/utils/helpers";
+import MetaSEO from "@/components/UI/MetaSEO";
 
 interface IPostContent {
   source: MDXRemoteSerializeResult;
@@ -19,18 +20,24 @@ const Blog = (props: IBlogPost) => {
   const { post } = props;
 
   return (
-    <div>
-      <h1 className="font-display text-5xl leading-[60px] mb-6">
-        {post.header.title}
-      </h1>
-      <p className="text-gray-400 font-light">
-        {helpers.dateFormatter(post.header.publishedAt)} •{" "}
-        <span>{post.header.readTime?.text}</span>
-      </p>
-      <div className="prose my-10">
-        <MDXRemote {...post.source} components={MDXComponents} />
+    <>
+      <MetaSEO
+        title={`${post.header.title} | Vaishnav's Notebook`}
+        description={post.header.summary}
+      />
+      <div>
+        <h1 className="font-display text-5xl leading-[60px] mb-6">
+          {post.header.title}
+        </h1>
+        <p className="text-gray-400 font-light">
+          {helpers.dateFormatter(post.header.publishedAt)} •{" "}
+          <span>{post.header.readTime?.text}</span>
+        </p>
+        <div className="prose my-10">
+          <MDXRemote {...post.source} components={MDXComponents} />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,42 +3,50 @@ import { GetStaticProps } from "next";
 import blogService from "@/lib/blog.services";
 import helpers from "@/utils/helpers";
 import { TPostFrontmatter, TPosts } from "@/utils/global.types";
+import MetaSEO from "@/components/UI/MetaSEO";
 
 const Home = (props: TPosts) => {
   const { posts } = props;
 
   return (
-    <div>
-      <div className="mb-10">
-        <h1 className="text-5xl mb-5 font-display">Blog</h1>
-        <p className="text-gray-400 text-base font-light">
-          Writings about my journey as a developer, learning through the
-          products I build, and life experiences.
-        </p>
-      </div>
+    <>
+      <MetaSEO
+        title="Vaishnav's NoteBook"
+        description="Writings about my journey as a developer, learning through the
+            products I build, and life experiences."
+      />
+      <div>
+        <div className="mb-10">
+          <h1 className="text-5xl mb-5 font-display">Blog</h1>
+          <p className="text-gray-500 text-base font-light leading-7">
+            Writings about my journey as a developer, learning through the
+            products I build, and life experiences.
+          </p>
+        </div>
 
-      <ul>
-        {posts.map((post: TPostFrontmatter) => (
-          <li key={post.slug}>
-            <Link
-              href={`/blog/${post.slug}`}
-              className=" border-b border-gray-100 block py-4 group"
-            >
-              <h2 className="text-[28px] font-display group-hover:text-purple-500 transition-all duration-200">
-                {post.title}
-              </h2>
-              <div className="text-sm font-light text-gray-400 mt-2">
-                <p className="mb-4 text-sm">
-                  {helpers.dateFormatter(post.publishedAt)} •{" "}
-                  <span>{post.readTime.text}</span>
-                </p>
-                <p>{post.summary}</p>
-              </div>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+        <ul>
+          {posts.map((post: TPostFrontmatter) => (
+            <li key={post.slug}>
+              <Link
+                href={`/blog/${post.slug}`}
+                className=" border-b border-gray-100 block py-4 group"
+              >
+                <h2 className="text-[28px] font-normal tracking-tight font-display group-hover:text-purple-500 transition-all duration-200">
+                  {post.title}
+                </h2>
+                <div className="text-sm text-gray-500 mt-2">
+                  <p className="mb-4 text-sm">
+                    {helpers.dateFormatter(post.publishedAt)} •{" "}
+                    <span>{post.readTime.text}</span>
+                  </p>
+                  <p>{post.summary}</p>
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
 
