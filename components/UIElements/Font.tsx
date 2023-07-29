0f9d6a5799adf3cf32c6bafb4dc86eb814e84bbf
Vaishnav <vaishnavchandurkar12@gmail.com>
2023-07-29 10:20:53 +0530

    clean up

--- components/UIElements/Font.tsx
+++
@@ -1,8 +0,0 @@
-import { Rufina } from "next/font/google";
-
-const rufina = Rufina({
-  subsets: ["latin"],
-  weight: ["400"],
-});
-
-export default rufina;
--- utils/types/index.ts
+++
@@ -1,4 +0,0 @@
-import { Database } from "./notion.types";
-import { Posts, Post } from "./posts.types";
-
-export type { Database, Posts, Post }
