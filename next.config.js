5432c9a7920d32559a3c0a8ca5c2ba277cb6a663
Vaishnav <vaishnavchandurkar12@gmail.com>
2023-08-14 18:01:32 +0530

    chores:update config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  assetPrefix: "https://vaishnavs.vercel.app",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
