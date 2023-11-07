e5da9804f9ce90f7b5a08def8db8c7800705c015
Vaishnav <vaishnavchandurkar12@gmail.com>
2023-11-07 10:35:27 +0530

    add: use notion as CMS

--- lib/notion.services.ts
+++ lib/notion.services.ts
@@ -1,9 +1,19 @@
 const { Client } = require("@notionhq/client");
+const { NotionToMarkdown } = require("notion-to-md");
+import matter from "gray-matter";
+import { serialize } from "next-mdx-remote/serialize";
+import remarkGfm from "remark-gfm";
+import rehypeSlug from "rehype-slug";
+import rehypeCodeTitles from "rehype-code-titles";
+import rehypePrism from "rehype-prism-plus";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import { TPostFrontmatter } from "@/utils/global.types";
 
 const notion = new Client({
   auth: process.env.NOTION_SECRET,
-})
+});
+
+const n2m = new NotionToMarkdown({ notionClient: notion });
 
 const notionServices = {
   getAllPublished: async() => {
@@ -27,6 +37,54 @@ const notionServices = {
     return allPosts;
   },
 
+  getPostBySlug: async(slug: string) => {
+    const response = await notion.databases.query({
+      database_id: process.env.NOTION_DATABASE_ID,
+      filter: {
+        property: "slug",
+        formula: {
+          string: {
+            equals: slug,
+          },
+        },
+      },
+    });
+
+    const notionPage = response.results[0];
+    const frontMatter = notionServices.getPageFrontmatter(notionPage);
+
+    const markdownBlocks = await n2m.pageToMarkdown(notionPage.id);
+    const markdownString = n2m.toMarkdownString(markdownBlocks);
+
+    const { content } = matter(markdownString.parent);
+
+    const mdxSource = await serialize(content, {
+      mdxOptions: {
+        remarkPlugins: [remarkGfm],
+        rehypePlugins: [
+          rehypeSlug,
+          rehypeCodeTitles,
+          rehypePrism,
+          [
+            rehypeAutolinkHeadings,
+            {
+              properties: {
+                className: ["anchor"],
+              },
+            },
+          ],
+        ],
+        format: "mdx",
+      },
+      scope: frontMatter,
+    });
+    
+    return {
+      header: frontMatter,
+      source: mdxSource
+    }
+  },
+
   getPageFrontmatter: (post: any) => {
     const frontMatter = {
       id: post.id, 
@@ -37,8 +95,7 @@ const notionServices = {
     }
 
     return frontMatter;
-  }
-
+  },
 
 }
 
--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -2,10 +2,10 @@ import { GetStaticPaths, GetStaticProps } from "next";
 import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
 import { ParsedUrlQuery } from "querystring";
 import MDXComponents from "@/components/MDXComponent";
-import blogService from "@/lib/blog.services";
 import { TPostFrontmatter } from "@/utils/global.types";
 import helpers from "@/utils/helpers";
 import MetaSEO from "@/components/UI/MetaSEO";
+import notionServices from "@/lib/notion.services";
 
 interface IPostContent {
   source: MDXRemoteSerializeResult;
@@ -23,7 +23,7 @@ const Blog = (props: IBlogPost) => {
     <>
       <MetaSEO
         title={`${post.header.title} | Vaishnav's Notebook`}
-        description={post.header.summary}
+        description={post.header.description}
       />
       <div>
         <h1 className="font-display text-5xl leading-[60px] mb-6">
@@ -31,7 +31,6 @@ const Blog = (props: IBlogPost) => {
         </h1>
         <p className="text-gray-500 text-sm">
           {helpers.dateFormatter(post.header.publishedAt)} •{" "}
-          <span>{post.header.readTime?.text}</span>
         </p>
         <div className="prose my-10">
           <MDXRemote {...post.source} components={MDXComponents} />
@@ -48,9 +47,9 @@ interface IParams extends ParsedUrlQuery {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const posts = await blogService.getAllPublished();
+  const posts = await notionServices.getAllPublished();
 
-  const paths = posts.map((post) => ({
+  const paths = posts.map((post: TPostFrontmatter) => ({
     params: {
       slug: post.slug,
     },
@@ -66,7 +65,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
   const { slug } = context.params as IParams;
 
   try {
-    const post = await blogService.getPostBySlug(slug);
+    const post = await notionServices.getPostBySlug(slug);
 
     return {
       props: {
@@ -74,7 +73,6 @@ export const getStaticProps: GetStaticProps = async (context) => {
       },
     };
   } catch (err) {
-    console.log("ERROR: ", err);
     return {
       notFound: true,
     };
